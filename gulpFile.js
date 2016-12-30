fc1d61ffe0faa7f2977a2350cef231e3223e8728
gesilar <huangjinguang@jimmys-macbook.local>
2016-12-30 20:04:30 +0800

    adapt to phone

--- gulpFile.js
+++ gulpFile.js
@@ -1,14 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-
-gulp.task('cssIndex',function() {
-    gulp.src('src/main.less')
-        .pipe(less())
-        .pipe(gulp.dest('dist/styles'))
-})
-
-
-gulp.task('cssSearch',function() {
+gulp.task('css',function() {
     gulp.src('src/search.less')
         .pipe(less())
         .pipe(gulp.dest('dist/styles'))
